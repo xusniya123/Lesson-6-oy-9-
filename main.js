34127f169aaa1d534052c5518d69ea1979e202ee
xusniya123 <xusniya12345678@gmail.com>
2024-06-07 16:24:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const currencyOne = document.getElementById("currency-one");
+const currencyTwo = document.getElementById("currency-two");
+const amountOne = document.getElementById("amount-one");
+ const amountTwo = document.getElementById("amount-two");
+ const reteEl = document.getElementById ("rate");
+ const swap = document.getElementById("swap");
+
+
+ function  calculate () {
+    const currency_one = currencyOne.value;
+    const currency_two = currencyTwo.value;
+
+    fetch(`
+    https://v6.exchangerate-api.com/v6/db9dd06ef07d2112693c914d/latest/${currency_one}`
+)
+.then((res) => res.json())
+.then((data) => {
+    const rate = data.conversion_rates[currency_two];
+
+    reteEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+    amountTwo.value = (amountOne.value * rate).toFixed(2);
+});
+ }
+
+currencyOne.addEventListener ("change", calculate);
+amountOne.addEventListener("input" , calculate);
+currencyTwo.addEventListener("change" , calculate);
+amountTwo.addEventListener("input" , calculate);
+
+swap.addEventListener("click" ,() =>{
+    const temp = currencyOne.value;
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = temp;
+    calculate();
+});
+
+calculate();
